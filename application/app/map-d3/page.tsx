71842b6b3e41cbe3b2c447342a8a187e3d5319ff
OctaveLT <ole_tullier@orange.fr>
2025-02-17 12:16:17 +0100

    feat: add d3 map in its own page

---
+++ application/app/map-d3/page.tsx
@@ -0,0 +1,9 @@
+import MapD3 from "../components/MapD3";
+
+export default function Home() {
+  return (
+    <div className="global-margin flex h-screen flex-col items-center justify-center">
+      <MapD3 />
+    </div>
+  );
+}
