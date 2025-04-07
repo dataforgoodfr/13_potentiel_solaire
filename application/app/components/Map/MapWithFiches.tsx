b949206af2f8b22f6ec8412665568ec9775c9c90
OctaveLT <ole_tullier@orange.fr>
2025-04-07 10:49:20 +0200

    fix: merge conflict

--- application/app/components/Map/MapWithFiches.tsx
+++ application/app/components/Map/MapWithFiches.tsx
@@ -6,15 +6,22 @@ import { EtablissementFeature } from '@/app/models/etablissements';
 
 import Fiches from '../fiches/Fiches';
 import FranceMap from './FranceMap';
+import Legend from './Legend/Legend';
+import { COLOR_THRESHOLDS } from './constants';
+import { Level } from './interfaces';
 
 export default function MapWithFiches() {
+	const [level, setLevel] = useState<Level>('regions');
 	const [selectedEtablissement, setSelectedEtablissement] = useState<EtablissementFeature | null>(
 		null,
 	);
 
 	return (
-		<>
-			<FranceMap onSelect={setSelectedEtablissement} />
+		<div className='relative h-full w-full'>
+			<FranceMap onSelect={setSelectedEtablissement} onLevelChange={setLevel} />
+			<div className='absolute bottom-2 left-2'>
+				<Legend thresholds={COLOR_THRESHOLDS[level]} />
+			</div>
 			{selectedEtablissement && (
 				<Fiches
 					etablissement={{
@@ -25,6 +32,6 @@ export default function MapWithFiches() {
 					onClose={() => setSelectedEtablissement(null)}
 				/>
 			)}
-		</>
+		</div>
 	);
 }
