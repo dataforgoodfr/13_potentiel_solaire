a4ccfd2ffd09ba075840cf80ad9c62145d14be02
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-04-08 13:53:55 +0400

    feat: fiche with proper tabs + fiche Commune

--- application/app/components/fiches/Fiches.tsx
+++ application/app/components/fiches/Fiches.tsx
@@ -3,17 +3,11 @@ import { useState } from 'react';
 import { Etablissement } from '@/app/models/etablissements';
 import { X } from 'lucide-react';
 
+import FicheCommune from './ficheCommune';
 import FicheEtablissement from './ficheEtablissement/ficheEtablissement';
 
 type TabId = 'region' | 'departement' | 'commune' | 'etablissement';
 type Tab = { id: TabId; label: string }[];
-
-const tabs: Tab = [
-	{ id: 'region', label: 'Région' },
-	{ id: 'departement', label: 'Département' },
-	{ id: 'commune', label: 'Commune' },
-	{ id: 'etablissement', label: 'Établissement' },
-];
 
 interface FichesProps {
 	etablissement?: Etablissement;
@@ -24,6 +18,26 @@ export default function Fiches({ etablissement, onClose }: FichesProps) {
 	const [activeTab, setActiveTab] = useState<TabId>('etablissement');
 
 	if (!etablissement) return null;
+
+	const tabs: Tab = [
+		{ id: 'region', label: etablissement?.libelle_region || 'Région' },
+		{ id: 'departement', label: etablissement?.libelle_departement || 'Département' },
+		{ id: 'commune', label: etablissement?.nom_commune || 'Commune' },
+		{ id: 'etablissement', label: etablissement?.nom_etablissement || 'Établissement' },
+	];
+
+	const commune = {
+		code_commune: etablissement.code_commune,
+		nom_commune: etablissement.nom_commune,
+		code_departement: etablissement.code_departement,
+		libelle_departement: etablissement.libelle_departement,
+		code_region: etablissement.code_region,
+		libelle_region: etablissement.libelle_region,
+		surface_utile: etablissement.surface_utile ?? 0,
+		potentiel_solaire: etablissement.potentiel_solaire ?? 0,
+		count_etablissements: 0,
+		count_etablissements_proteges: 0,
+	};
 
 	return (
 		<div className='fixed right-0 top-0 z-50 h-full w-full max-w-sm overflow-y-auto bg-white pl-5 pt-1 shadow-lg md:w-96'>
@@ -49,7 +63,7 @@ export default function Fiches({ etablissement, onClose }: FichesProps) {
 			<div className='p-4'>
 				{activeTab === 'region' && <div>Contenu onglet région</div>}
 				{activeTab === 'departement' && <div>Contenu onglet département</div>}
-				{activeTab === 'commune' && <div>Contenu onglet commune</div>}
+				{activeTab === 'commune' && <FicheCommune commune={commune} />}
 				{activeTab === 'etablissement' && (
 					<FicheEtablissement feature={etablissement} onClose={onClose} />
 				)}
