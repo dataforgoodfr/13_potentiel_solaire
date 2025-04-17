02b6b985e2849ad370387441a268a00f167e619e
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-04-17 14:11:50 +0400

    chore: take out const in TopCard

--- application/app/components/fiches/shared/TopCard.tsx
+++ application/app/components/fiches/shared/TopCard.tsx
@@ -6,6 +6,8 @@ const UNKNOWN_TEXTS = {
 	top_etablissement: 'Aucun établissement mis en avant pour cette collectivité.',
 };
 
+const medals = ['🥇', '🥈', '🥉'];
+
 type Etablissement = {
 	id: string;
 	libelle: string;
@@ -17,10 +19,8 @@ type Props = {
 };
 
 const TopCard = ({ topEtablissements }: Props) => {
-	const medals = ['🥇', '🥈', '🥉'];
-
 	if (!topEtablissements || topEtablissements.length === 0) {
-		return <p className='italic text-gray-500'>{UNKNOWN_TEXTS.top_etablissement}</p>;
+		return <p className='text-gray-500 italic'>{UNKNOWN_TEXTS.top_etablissement}</p>;
 	}
 
 	return (
@@ -32,7 +32,7 @@ const TopCard = ({ topEtablissements }: Props) => {
 			<ul className='list-none space-y-1 pl-0 font-bold text-darkgreen'>
 				{topEtablissements.slice(0, 3).map((etab, index) => (
 					<li key={etab.id}>
-						{medals[index] ?? '🏅'}{' '}
+						{medals[index]}{' '}
 						<Link
 							// the href might need to be corrected
 							href={`/etablissement/${etab.id}`}
