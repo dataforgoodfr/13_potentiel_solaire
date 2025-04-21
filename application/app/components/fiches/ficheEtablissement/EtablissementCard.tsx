72de48c7fefa715ab717a1b032806f2e6ab4f211
Jean Bui Quang <jean.buiquang@etik.com>
2025-04-21 12:47:48 +0200

    feat: add etab adresse todo

--- application/app/components/fiches/ficheEtablissement/EtablissementCard.tsx
+++ application/app/components/fiches/ficheEtablissement/EtablissementCard.tsx
@@ -10,6 +10,7 @@ const UNKNOWN_TEXTS = {
 	adresse: 'Adresse inconnue',
 };
 
+//TODO: if adresse_3 is null => use code_postal + nom_commune (à confirmer au niveau data)
 const EtablissementCard = ({
 	nom_etablissement,
 	adresse_1,
@@ -20,8 +21,8 @@ const EtablissementCard = ({
 		<div className='text-blue'>
 			<h1 className='text-2xl font-bold'>{nom_etablissement || UNKNOWN_TEXTS.name}</h1>
 			<p>{adresse_1 || UNKNOWN_TEXTS.adresse}</p>
-			<p>{adresse_2 || ''}</p>
-			<p>{adresse_3 || ''}</p>
+			{adresse_2 && <p>{adresse_2}</p>}
+			{adresse_3 && <p>{adresse_3}</p>}
 		</div>
 	);
 };
