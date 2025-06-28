fd242430cc62531420b9ec9e1359a575f4d0cf34
machbry <matthieu.chambery@gmail.com>
2025-06-28 14:29:14 +0200

    add github action to build app docker image

--- application/publish_image.sh
+++ application/publish_image.sh
@@ -6,11 +6,13 @@ APPLICATION_DIR=$(dirname "$(readlink -f "$0")")
 # Get the current date
 ALGORITHME_VERSION="0.1.0"
 CURRENT_DATE=$(date +%Y%m%d)
-IMAGE_NAME="ghcr.io/dataforgoodfr/13_potentiel_solaire_app:$ALGORITHME_VERSION.$CURRENT_DATE"
+IMAGE_NAME="ghcr.io/dataforgoodfr/13_potentiel_solaire_app"
+TAG="$ALGORITHME_VERSION.$CURRENT_DATE"
 
 # Build docker image
 docker build \
-	-t $IMAGE_NAME \
+	-t $IMAGE_NAME:$TAG \
+    -t $IMAGE_NAME:latest \
 	--build-arg DATABASE_PATH=/app/database/data.duckdb \
 	-f $APPLICATION_DIR/Dockerfile .
 
@@ -23,9 +25,8 @@ else
 
     # Créer un token personnel sur son compte github, avec comme droit : write:packages
     # cf https://docs.github.com/fr/packages/working-with-a-github-packages-registry/working-with-the-container-registry
-    read -p "Enter your personal access token (github) with write:packages scope: " CR_PAT
-    echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
+    docker login ghcr.io -u USERNAME -p $GHCR_TOKEN
 
     # Push the image to GitHub Container Registry
-    docker push $IMAGE_NAME
+    docker push $IMAGE_NAME --all-tags
 fi
