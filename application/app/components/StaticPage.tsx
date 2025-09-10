06197dd488dfbd58aefcd73dec52f59c3188ef33
Julia Hu <juliahukoshi@gmail.com>
2025-09-10 16:07:09 +0200

    a propos page routing et contenu

--- application/app/components/StaticPage.tsx
+++ application/app/components/StaticPage.tsx
@@ -1,39 +1,65 @@
+import { JSX } from 'react';
+
+import Image from 'next/image';
+
+import clsx from 'clsx';
+
 export type StaticPageProps = {
 	title: string;
+	picture?: { src: string; alt: string; width: number };
 	sections: {
 		heading: string;
-		paragraphs: string[];
+		paragraphs: Array<string | JSX.Element>;
 	}[];
 };
 
-const StaticPage = ({ title, sections }: StaticPageProps) => (
-	<article className='mx-auto max-w-3xl px-4 py-8'>
-		<h1
-			className='mb-4 text-[24px] font-bold leading-[28px] tracking-normal text-white'
-			style={{ textDecoration: 'none' }}
+const StaticPage = ({ title, sections, picture }: StaticPageProps) => {
+	console.log(!!picture);
+	return (
+		<article
+			className={clsx('mx-auto px-4 py-8', !!picture && 'max-w-6xl', !picture && 'max-w-3xl')}
 		>
-			{title}
-		</h1>
-		{sections.map((section, idx) => (
-			<section key={idx} className='mb-6'>
-				<h2
-					className='mb-2 text-[16px] font-bold leading-normal tracking-[-0.03em] text-sol_ko'
-					style={{ textDecoration: 'none' }}
-				>
-					{section.heading}
-				</h2>
-				{section.paragraphs.map((p, i) => (
-					<p
-						key={i}
-						className='mb-2 text-[14px] font-normal leading-[28px] tracking-normal text-sol_ko'
-						style={{ textDecoration: 'none' }}
-					>
-						{p}
-					</p>
+			<h1
+				className='mb-4 text-[24px] font-bold leading-[28px] tracking-normal text-white'
+				style={{ textDecoration: 'none' }}
+			>
+				{title}
+			</h1>
+
+			<div className='flex flex-col gap-4 md:flex-row-reverse'>
+				{picture && (
+					<div className='h-16 w-full md:w-1/2'>
+						<Image
+							src={picture.src}
+							alt={picture.alt}
+							width={picture.width}
+							height={190}
+						/>
+					</div>
+				)}
+
+				{sections.map((section, idx) => (
+					<section key={idx} className='mb-6 w-full md:w-1/2'>
+						<h2
+							className='mb-2 text-[16px] font-bold leading-normal tracking-[-0.03em] text-sol_ko'
+							style={{ textDecoration: 'none' }}
+						>
+							{section.heading}
+						</h2>
+						{section.paragraphs.map((p, i) => (
+							<p
+								key={i}
+								className='mb-2 text-[14px] font-normal leading-[28px] tracking-normal text-sol_ko'
+								style={{ textDecoration: 'none' }}
+							>
+								{p}
+							</p>
+						))}
+					</section>
 				))}
-			</section>
-		))}
-	</article>
-);
+			</div>
+		</article>
+	);
+};
 
 export default StaticPage;
