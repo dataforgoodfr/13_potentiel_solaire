211d7a34e3f3244554f1943a4ef2d7f39887357e
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-09-12 23:26:09 +0200

    [FRONT] Only show tabs for loaded data

--- application/app/components/fiches/Fiches.tsx
+++ application/app/components/fiches/Fiches.tsx
@@ -98,14 +98,14 @@ export default function Fiches({
 	}
 
 	const tabs: Tab = [
-		{ id: 'region', label: LEVEL_TO_LABEL_SHORTENED['region'] },
-		{ id: 'departement', label: LEVEL_TO_LABEL_SHORTENED['departement'] },
-		{ id: 'commune', label: LEVEL_TO_LABEL_SHORTENED['commune'] },
-		{ id: 'etablissement', label: etablissement?.type_etablissement },
-	];
+    ...(region ? [{ id: 'region' as const, label: LEVEL_TO_LABEL_SHORTENED['region'] }] : []),
+    ...(departement ? [{ id: 'departement' as const, label: LEVEL_TO_LABEL_SHORTENED['departement'] }] : []),
+    ...(commune ? [{ id: 'commune' as const, label: LEVEL_TO_LABEL_SHORTENED['commune'] }] : []),
+    ...(etablissement ? [{ id: 'etablissement' as const, label: etablissement.type_etablissement }] : []),
+  ];
 
 	const filteredTabs = tabs.filter((tab) => tab.label !== undefined);
-
+  
 	return (
 		<div
 			ref={ficheContainerRef}
