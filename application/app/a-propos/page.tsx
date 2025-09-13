3571692b1ead623a75a224075db1ac68a38a6b94
Julia Hu <juliahukoshi@gmail.com>
2025-09-13 14:36:50 +0200

    picture format and fallback + nation to pays

--- application/app/a-propos/page.tsx
+++ application/app/a-propos/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 import Image from 'next/image';
 
@@ -14,16 +14,23 @@ import { A_PROPOS_DATA_FOR_GOOD, A_PROPOS_GREENPEACE } from '../components/conte
 const titleStyle = 'flex h-16 w-full items-center justify-between px-16';
 const contentStyle = 'px-16 pb-4 leading-relaxed tracking-normal text-sol_ko';
 
-const getActionsLong = (isOpen: boolean[]) => [
+const getActionsLong = (
+	isOpen: boolean[],
+	greenpeaceImgSrc: string,
+	dataForGoodImgSrc: string,
+	setGreenpeaceImgSrc: Dispatch<SetStateAction<string>>,
+	setDataForGoodImgSrc: Dispatch<SetStateAction<string>>,
+) => [
 	{
 		title: (
 			<div className={titleStyle}>
 				<Image
-					src='/images/greenpeaceLong.png'
+					src={greenpeaceImgSrc}
 					alt='Logo de Greenpeace'
 					width={261}
 					height={37}
 					className='h-16 w-auto object-contain'
+					onError={() => setGreenpeaceImgSrc('/images/greenpeaceLong.png')}
 				/>
 				{!isOpen[0] && <SquarePlus className='text-white' />}
 				{isOpen[0] && <SquareMinus className='text-white' />}
@@ -35,12 +42,13 @@ const getActionsLong = (isOpen: boolean[]) => [
 		title: (
 			<div className={titleStyle}>
 				<Image
-					src='/images/dataForGoodLong.png'
+					src={dataForGoodImgSrc}
 					alt='Logo de Data for good'
 					width={126}
 					height={48}
 					style={{ borderRadius: '30px' }}
 					className='h-12 w-auto object-contain'
+					onError={() => setDataForGoodImgSrc('/images/dataForGoodLong.png')}
 				/>
 				{!isOpen[1] && <SquarePlus className='text-white' />}
 				{isOpen[1] && <SquareMinus className='text-white' />}
@@ -52,12 +60,22 @@ const getActionsLong = (isOpen: boolean[]) => [
 
 export default function AProposPage() {
 	const [openStates, setOpenStates] = useState<boolean[]>([false, false]);
+	const [greenpeaceImgSrc, setGreenpeaceImgSrc] = useState<string>('/images/greenpeaceLong.webp');
+	const [dataForGoodImgSrc, setDataForGoodImgSrc] = useState<string>(
+		'/images/dataForGoodLong.webp',
+	);
 	return (
 		<>
 			<main className='mx-auto mb-24 flex max-w-6xl flex-col justify-evenly py-8'>
 				<StaticPage {...aProposContent} />
 
-				{getActionsLong(openStates).map((action, index) => (
+				{getActionsLong(
+					openStates,
+					greenpeaceImgSrc,
+					dataForGoodImgSrc,
+					setGreenpeaceImgSrc,
+					setDataForGoodImgSrc,
+				).map((action, index) => (
 					<Collapsible
 						key={index}
 						asChild
@@ -69,7 +87,9 @@ export default function AProposPage() {
 						}}
 					>
 						<span className='mb-4 w-full max-w-6xl bg-[#151528]'>
-							<CollapsibleTrigger className="flex w-full justify-between">{action.title}</CollapsibleTrigger>
+							<CollapsibleTrigger className='flex w-full justify-between'>
+								{action.title}
+							</CollapsibleTrigger>
 							<CollapsibleContent>{action.content}</CollapsibleContent>
 						</span>
 					</Collapsible>
