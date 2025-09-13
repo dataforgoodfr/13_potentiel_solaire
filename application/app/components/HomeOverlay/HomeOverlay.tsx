262571a994bd4942b555258e969431e1ffdc9ff5
Julia Hu <juliahukoshi@gmail.com>
2025-09-13 15:13:15 +0200

    max width navbar et overlay

--- application/app/components/HomeOverlay/HomeOverlay.tsx
+++ application/app/components/HomeOverlay/HomeOverlay.tsx
@@ -9,30 +9,32 @@ export default function HomeOverlay() {
 	return (
 		<div className='absolute inset-0 z-40 flex h-full w-full flex-col items-center justify-start bg-blue/80 p-4'>
 			{/* TODO: text color should be gray-6 */}
-			<div className='mx-auto max-w-screen-xl px-6 pb-6 pt-4'>
-				<h1 className='mb-24 inline-block font-verdana text-[28px] font-normal leading-normal tracking-sm text-slate-100 '>
+			<div className='w-full max-w-screen-xl pb-6 pt-4'>
+				<h1 className='mb-24 inline-block font-verdana text-[28px] font-normal leading-normal tracking-sm text-slate-100'>
 					Découvrez le
 					<br />
 					<strong>potentiel solaire</strong> 🔆
 					<br />
 					de votre <strong>école</strong> 🏫
 				</h1>
-				<div className='flex max-w-[434px] shrink-0 flex-col items-center justify-center rounded-[8px] border border-light-green bg-blue/80 py-[30px] shadow-base'>
-					<div className='px-3'>
-						<h2 className='mb-4 text-base font-normal leading-6 text-white'>
-							Saisir une région, un département, une commune ou le nom d&#39;un
-							établissement :
-						</h2>
-						<SearchBar onSelect={closeInitialView} />
-					</div>
-					<hr className='my-5 h-[1px] w-full border-light-green' />
-					<div className='flex items-center justify-center'>
-						<button
-							className='rounded-md bg-light-green px-4 py-2 text-sm font-bold leading-6 text-darkgreen'
-							onClick={closeInitialView}
-						>
-							Je préfère utiliser la carte
-						</button>
+				<div className='w-full flex justify-center'>
+					<div className='flex max-w-[434px] shrink-0 flex-col items-center justify-center rounded-[8px] border border-light-green bg-blue/80 py-[30px] shadow-base'>
+						<div className='px-3'>
+							<h2 className='mb-4 text-base font-normal leading-6 text-white'>
+								Saisir une région, un département, une commune ou le nom d&#39;un
+								établissement :
+							</h2>
+							<SearchBar onSelect={closeInitialView} />
+						</div>
+						<hr className='my-5 h-[1px] w-full border-light-green' />
+						<div className='flex items-center justify-center'>
+							<button
+								className='rounded-md bg-light-green px-4 py-2 text-sm font-bold leading-6 text-darkgreen'
+								onClick={closeInitialView}
+							>
+								Je préfère utiliser la carte
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
