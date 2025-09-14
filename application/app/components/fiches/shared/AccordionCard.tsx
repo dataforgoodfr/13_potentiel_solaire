ea40a8ca149353e5da9b1c39bfc07c0f2eed2f9e
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-09-14 13:30:33 +0200

    feat: Add content to Comment agir page

--- application/app/components/fiches/shared/AccordionCard.tsx
+++ application/app/components/fiches/shared/AccordionCard.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-
 import { SquareMinus, SquarePlus } from 'lucide-react';
 
 import {
@@ -21,31 +20,34 @@ export default function AccordionCard({ actions }: AccordionCardProps) {
 	return (
 		<div>
 			{actions.map((action, index) => (
-				<Collapsible
-					key={index}
-					className='mb-2 rounded-md border border-blue bg-black p-2 text-sm text-white'
-				>
-					<CollapsibleTrigger className='flex w-full items-center justify-between px-4 py-2 text-sm font-bold'>
-						<span>{action.title}</span>
-						<CollapsibleIcon />
-					</CollapsibleTrigger>
-					<CollapsibleContent className='mt-2 block'>{action.content}</CollapsibleContent>
-				</Collapsible>
+				<CollapsibleItem key={index} action={action} />
 			))}
 		</div>
 	);
 }
 
-function CollapsibleIcon() {
+function CollapsibleItem({ action }: { action: { title: string; content: React.ReactNode } }) {
 	const [open, setOpen] = useState(false);
 
 	return (
-		<Collapsible onOpenChange={setOpen}>
-			{open ? (
-				<SquareMinus className='h-5 w-5 shrink-0 stroke-green' />
-			) : (
-				<SquarePlus className='h-5 w-5 shrink-0 stroke-green' />
-			)}
+		<Collapsible
+			open={open}
+			onOpenChange={setOpen}
+			className='mb-2 rounded-md border border-blue bg-black p-2 text-sm text-white'
+		>
+			<CollapsibleTrigger className='flex w-full items-center justify-between px-4 py-2 text-sm font-bold'>
+				<span>{action.title}</span>
+				<CollapsibleIcon open={open} />
+			</CollapsibleTrigger>
+			<CollapsibleContent className='mt-2 block'>{action.content}</CollapsibleContent>
 		</Collapsible>
+	);
+}
+
+function CollapsibleIcon({ open }: { open: boolean }) {
+	return open ? (
+		<SquareMinus className='h-5 w-5 shrink-0 stroke-green' />
+	) : (
+		<SquarePlus className='h-5 w-5 shrink-0 stroke-green' />
 	);
 }
