8a64329dc43a200a1ef35c5ee5690e0239fd2367
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-09-22 20:10:37 +0200

    [FRONT] Feature/web EAA compliance

--- application/app/components/Error.tsx
+++
@@ -1,7 +0,0 @@
-export default function Error() {
-	return (
-		<section className='h-[100%] w-[100%] text-center' role='alert' aria-live='assertive'>
-			Erreur lors du chargement des données.
-		</section>
-	);
-}
