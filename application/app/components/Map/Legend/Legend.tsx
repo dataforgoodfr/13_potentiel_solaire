b9ec7175a028d0e17d870308e4d5f4f2ff7bfdc1
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-09-23 19:51:00 +0200

    [FRONT] Feature/web EAA compliance

--- application/app/components/Map/Legend/Legend.tsx
+++ application/app/components/Map/Legend/Legend.tsx
@@ -24,11 +24,15 @@ export default function Legend({ thresholds }: Legend) {
 	const lastThreshold = Number(Object.keys(thresholds).slice(-1)[0]);
 	const lastThresholdUnit = getClosestEnergyUnit(lastThreshold);
 	const thresholdValues = Object.entries(thresholds);
+	const t0 = Math.round(convertKWhTo(Number(thresholdValues[0][0]), lastThresholdUnit));
+	const t1 = Math.round(convertKWhTo(Number(thresholdValues[1][0]), lastThresholdUnit));
+	const t2 = Math.round(convertKWhTo(Number(thresholdValues[2][0]), lastThresholdUnit));
+
 	const ariaLabel = `
 		Légende du potentiel solaire.
-		De 0 à ${thresholdValues[1][0]} ${lastThresholdUnit}/an : Potentiel solaire limité.
-		De ${thresholdValues[1][0]} à ${thresholdValues[2][0]} ${lastThresholdUnit}/an : Potentiel solaire bon.
-		À partir de ${thresholdValues.slice(-1)[0][0]} ${lastThresholdUnit}/an : Potentiel solaire élevé.
+		De ${t0} à ${t1} ${lastThresholdUnit}/an : Potentiel solaire limité.
+		De ${t1} à ${t2} ${lastThresholdUnit}/an : Potentiel solaire bon.
+		À partir de ${t2} ${lastThresholdUnit}/an : Potentiel solaire élevé.
 	`;
 
 	return (
