1d4f31282427626aedc92e1a0fd65340a004fb92
Jean Bui Quang <jean.buiquang@etik.com>
2025-10-12 18:32:57 +0200

    fix: use runtime env var

--- application/app/robots.ts
+++ application/app/robots.ts
@@ -1,9 +1,8 @@
 import type { MetadataRoute } from 'next';
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-if (!baseUrl) throw new Error('NEXT_PUBLIC_BASE_URL must be set!');
-
 export default function robots(): MetadataRoute.Robots {
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	if (!baseUrl) throw new Error('NEXT_PUBLIC_BASE_URL must be set!');
 	return {
 		rules: [
 			{
