19999746c7fea60f2dbf6a14ce21a07e3b00ab6b
GaelleNS <43820187+GaelleNS@users.noreply.github.com>
2025-10-16 22:36:43 +0200

    Update algorithme/database/versions/2025_10_16_2232_correct_bad_contacts_urls.py

--- algorithme/database/versions/2025_10_16_2232_correct_bad_contacts_urls.py
+++ algorithme/database/versions/2025_10_16_2232_correct_bad_contacts_urls.py
@@ -24,7 +24,23 @@ def upgrade() -> None:
     """Upgrade schema."""
     op.execute(f"""
     UPDATE {annuaire_administration_table}
-    SET contact_url = contact_url
+SET url_contact = NULL
+WHERE lower(url_contact) IN ('mairie.', 'mairie');
+UPDATE {annuaire_administration_table}
+SET url_contact = substr(url_contact, 1, strpos(lower(url_contact), '.fr') + 2)
+                 || '/fr/'
+                 || substr(url_contact, strpos(lower(url_contact), '.fr') + 5)
+WHERE strpos(lower(url_contact), '.fr') > 0
+ AND substr(lower(url_contact), strpos(lower(url_contact), '.fr') + 3, 2) = 'fr'
+ AND url_contact != 'https://www.fresnes94.fr/contact/';
+UPDATE {annuaire_administration_table}
+SET url_contact = substr(url_contact, 1, strpos(lower(url_contact), '.fr') + 2)
+                 || '/'
+                 || substr(url_contact, strpos(lower(url_contact), '.fr') + 3)
+WHERE strpos(lower(url_contact), '.fr') > 0
+ AND substr(lower(url_contact), strpos(lower(url_contact), '.fr') + 3, 1) NOT IN ('', '/')
+ AND substr(lower(url_contact), strpos(lower(url_contact), '.fr') + 3, 2) != 'fr'
+ AND url_contact != 'https://www.fresnes94.fr/contact/';
     """)
 
 
